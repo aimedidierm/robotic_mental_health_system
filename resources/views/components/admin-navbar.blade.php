4972372f501cbdffd321f500ab133b23db91657f
aimedidierm <aimedidiermugisha@gmail.com>
2023-11-29 09:22:24 +0200

    Print patients model fix

--- resources/views/components/admin-navbar.blade.php
+++ resources/views/components/admin-navbar.blade.php
@@ -105,7 +105,8 @@
                         </a>
                     </li>
                     <li>
-                        <button type="button" data-modal-target="addNewModal" data-modal-toggle="addNewModal"
+                        <button type="button" data-modal-target="printPatientsModel"
+                            data-modal-toggle="printPatientsModel"
                             class="flex items-center p-2  rounded-lg dark:text-white hover:text-black hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <span class="material-symbols-outlined">
                                 group
@@ -136,7 +137,7 @@
         </aside>
     </div>
 </nav>
-<div id="addNewModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
+<div id="printPatientsModel" data-modal-backdrop="static" tabindex="-1" aria-hidden="true"
     class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative w-full max-w-2xl max-h-full">
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -146,7 +147,7 @@
                 </h3>
                 <button type="button"
                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="addNewModal">
+                    data-modal-hide="printPatientsModel">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
